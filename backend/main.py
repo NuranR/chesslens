68e1381c6954a68b4c4bcd520f9622ea0e1c6f80
NuranR <ranawakanuran@gmail.com>
2026-02-18 14:56:29 +0530

    docs: minor changes

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI(
 # Configure CORS to allow your frontend to communicate with the backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # The default port for Vite/React
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
